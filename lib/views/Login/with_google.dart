e1abde29c4c70f041742ef3ef87b7bf2d6fbd7de
neskinggoblin <fnik@gmail.com>
2024-02-07 13:58:38 +0700

    Move repo

--- lib/views/Login/with_google.dart
+++ lib/views/Login/with_google.dart
@@ -1,5 +1,5 @@
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 final GoogleSignIn _googleSignIn = GoogleSignIn();
 final FirebaseAuth _auth = FirebaseAuth.instance;
--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,9 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
 import 'Login/checkAuth.dart';
 import 'Login/with_google.dart';
-
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
